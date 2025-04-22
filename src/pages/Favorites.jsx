40230130b124e2a5795d194770c11cae173728ba
Shasen Shrestha <shasenshrestha3@gmail.com>
2025-04-22 18:28:34 +0545

    second comit

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../css/Favorites.css'
-import { useMovieContext } from '../contexts/Moviecontext'
+import { useMovieContext } from "../contexts/MovieContext";
+
 import MovieCard from '../components/MovieCard'
 
 const Favorites = () => {
